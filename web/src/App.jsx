7bab6e7316d3ca1189452b7a4a50b378f4c0809c
Đặng Hữu Thắng <hwthang.dev.2510@outlook.com>
2025-09-18 21:42:35 +0700

    Hwthang (#2) initialize auth feature
    * setup react-router-dom
    
    * initial auth feature

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import AuthRoutes from "./features/auth/AuthRoutes";
 
 function App() {
-  return <div className="text-5xl text-red-600">App</div>;
+  const appRoutes = [<AuthRoutes />];
+  return (
+    <>
+      {appRoutes.map((routes, index) => (
+        <div key={index}>{routes}</div>
+      ))}
+    </>
+  );
 }
 
 export default App;
